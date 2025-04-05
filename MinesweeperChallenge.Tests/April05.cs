fd4efe0aeb3eab44d4f52234c599077e7cb923eb
Dimitri <dimitri.koch@agicap.com>
2025-04-05 19:28:23 +0200

    add examples

--- MinesweeperChallenge.Tests/April05.cs
+++ MinesweeperChallenge.Tests/April05.cs
@@ -1,5 +1,21 @@
 namespace MinesweeperChallenge.Tests;
 
+// Applying constraint: 1
+// Examples:
+//   * ->   *
+//   . ->  0
+//  ** ->  **
+//  .. ->  00
+// *** -> ***
+// ... -> 000
+//  *. ->  *1
+//  .* ->  1*
+// **. -> **1
+// .** -> 1**
+// *.. -> *10
+// ..* -> 01*
+// .*. -> 1*1
+// *.* -> *2*
 public class April05
 {
     [Fact]
